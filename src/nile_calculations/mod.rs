c1b16931d002cccf9628a9bb8234f1988549dff6
Sean McGary <sean@seanmcgary.com>
2024-10-11 12:55:19 -0500

    Nile operator tokens

--- src/nile_calculations/mod.rs
+++ src/nile_calculations/mod.rs
@@ -1,11 +1,20 @@
-use bigdecimal::{BigDecimal, Context};
+use bigdecimal::{BigDecimal};
 use std::str::FromStr;
-use serde_json::Value;
 
-pub fn nile_staker_token_rewards(sp: &str, tpd: &str) -> BigDecimal {
+pub fn nile_staker_token_rewards(sp: &str, tpd: &str) -> String {
     let staker_proportion = BigDecimal::from_str(sp).unwrap();
     let tokens_per_day = BigDecimal::from_str(tpd).unwrap();
 
     let final_prec = core::num::NonZeroU64::try_from(15).unwrap();
-    (staker_proportion * tokens_per_day).with_precision_round(final_prec, bigdecimal::RoundingMode::HalfUp)
+    (staker_proportion * tokens_per_day).with_precision_round(final_prec, bigdecimal::RoundingMode::HalfUp).to_string()
+}
+
+pub fn nile_operator_token_rewards(tsot: &str) -> String {
+    let total_staker_operator_tokens = BigDecimal::from_str(tsot).unwrap();
+    let operator_commission = BigDecimal::from_str(".10").unwrap();
+
+    let result = operator_commission * total_staker_operator_tokens;
+
+
+    result.with_scale_round(0, bigdecimal::RoundingMode::HalfUp).to_string()
 }
