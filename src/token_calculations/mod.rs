d2cd30dd2983d3c8207191f0c8e36561ed2d2712
Sean McGary <sean@seanmcgary.com>
2024-10-15 15:17:28 -0500

    Tokens per day almost working....

--- src/token_calculations/mod.rs
+++ src/token_calculations/mod.rs
@@ -1,7 +1,9 @@
-use bigdecimal::{BigDecimal};
+// use bigdecimal::{BigDecimal};
 use std::str::FromStr;
 use std::ffi::{CString, CStr};
 use std::os::raw::c_char;
+use num::bigint::*;
+use bigdecimal::*;
 
 /// Calculate the staker token rewards by multiplying the staker proportion by the tokens per day.
 pub fn staker_token_rewards(sp: &str, tpd: &str) -> String {
@@ -71,6 +73,112 @@ pub extern "C" fn staker_weight_c(mul: *const c_char, shares: *const c_char) -> 
     };
 
     let result = staker_weight(mul_str, shares_str);
+
+    CString::new(result).unwrap().into_raw()
+}
+
+pub fn tokens_per_day(amount: &str, duration: &str) -> String {
+    let amount_d = BigDecimal::from_str(amount).unwrap();
+    let duration_d = duration.parse::<i64>().unwrap();
+
+    let mut per_day: BigDecimal = (BigDecimal::from(duration_d) / 86400);
+    if per_day.is_integer() {
+        per_day = per_day.with_scale_round(0, RoundingMode::Down);
+    } else {
+        per_day = per_day.with_scale(28);
+    }
+    println!("per day:  {}", per_day);
+    let mut tpd = amount_d / per_day;
+    println!("tpd:      {}", tpd);
+
+    let mut scaled = false;
+    if tpd < BigDecimal::from(1) {
+        scaled = true;
+        tpd = tpd.with_scale_round(20, bigdecimal::RoundingMode::HalfEven);
+    }
+    println!("tpd 2:    {} (scaled {})", tpd, scaled);
+
+    // strip any trailing 0s
+    //tpd = tpd.normalized();
+
+    let mut scale = 16;
+    if tpd < BigDecimal::from(1) {
+        scale = 20;
+    }
+    println!("Pre scale digits: {}", tpd.digits());
+    let mut scaled_tpd = tpd.with_scale_round(scale, RoundingMode::HalfEven);
+    println!("scaled_tpd: {}", scaled_tpd);
+    println!("Digits: {}", scaled_tpd.digits());
+
+
+    if scaled_tpd.is_integer() {
+        return scaled_tpd.with_scale_round(0, RoundingMode::HalfEven).to_string();
+    }
+    // this kinda works
+    if scaled_tpd >= BigDecimal::from(1) {
+        let b = scaled_tpd.to_bigint().unwrap();
+        let left_side_digits = b.to_string().len();
+
+        if left_side_digits >= 18 {
+            println!("Truncating....");
+            return scaled_tpd.with_scale_round(0, RoundingMode::HalfEven).to_string();
+        }
+
+        
+        let mut decimal_digits: i64 = scaled_tpd.digits() as i64 - left_side_digits as i64;
+        if decimal_digits > 12 {
+            decimal_digits = 12;
+        }
+
+        let total_digits = left_side_digits as i64 + decimal_digits;
+
+        println!("Decimal digits: {}", decimal_digits);
+        scaled_tpd = scaled_tpd.with_scale_round(decimal_digits, RoundingMode::HalfEven);
+    }
+
+
+    scaled_tpd.to_string()
+}
+
+#[no_mangle]
+pub extern "C" fn tokens_per_day_c(amount: *const c_char, duration: *const c_char) -> *mut c_char {
+    let amount_str = unsafe {
+        assert!(!amount.is_null());
+        CStr::from_ptr(amount).to_str().unwrap()
+    };
+    let duration_str = unsafe {
+        assert!(!duration.is_null());
+        CStr::from_ptr(duration).to_str().unwrap()
+    };
+
+    let result = tokens_per_day(amount_str, duration_str);
+
+    CString::new(result).unwrap().into_raw()
+}
+
+pub fn tokens_per_day_decimal(amount: &str, duration: &str) -> String {
+    let amount = BigDecimal::from_str(amount).unwrap();
+    let duration = BigDecimal::from_str(duration).unwrap();
+
+    let per_day = duration / BigDecimal::from_str("86400").unwrap();
+
+    let tpd = (amount / per_day).with_prec(22);
+
+    tpd.to_string()
+}
+
+#[no_mangle]
+pub extern "C" fn tokens_per_day_decimal_c(amount: *const c_char, duration: *const c_char) -> *mut c_char {
+    let amount_str = unsafe {
+        assert!(!amount.is_null());
+        CStr::from_ptr(amount).to_str().unwrap()
+    };
+    let duration_str = unsafe {
+        assert!(!duration.is_null());
+        CStr::from_ptr(duration).to_str().unwrap()
+    };
+
+    let result = tokens_per_day_decimal(amount_str, duration_str);
 
     CString::new(result).unwrap().into_raw()
 }
